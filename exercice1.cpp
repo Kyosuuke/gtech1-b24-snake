952dcd499a6fed7dda104e4501614cba469cdc6d
Clement LI <cli@gaming.tech>
2022-01-21 10:32:13 +0100

    g

---
+++ exercice1.cpp
@@ -0,0 +1,5 @@
+#include <exercice1.hpp>
+
+int MainSDLWindow::Init(){
+
+}
---
+++ exercice1.hpp
@@ -0,0 +1,18 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+class MainSDLWindow
+{
+private:
+    
+public:
+ MainSDLWindow();
+     MainSDLWindow();
+};
+ MainSDLWindow: MainSDLWindow()
+{
+}
+ MainSDLWindow:: MainSDLWindow()
+{
+}
