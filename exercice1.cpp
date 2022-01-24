fb929c79da18121a2281f042fac43d80b52797db
Clement LI <cli@gaming.tech>
2022-01-24 09:50:37 +0100

    yes

--- exercice1.cpp
+++ exercice1.cpp
@@ -1,5 +1,27 @@
-#include <exercice1.hpp>
+#include "exercice1.hpp"
 
-int MainSDLWindow::Init(){
+int main(void){
 
+ int MainSDLWindow::Init(const char *windowName, int width, int height);{
+    this->window = SDL_CreateWindow(windowName,
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, width , height , SDL_WINDOW_RESIZABLE);  //Création de la fenêtre
+}
+
+SDL_Renderer* MainSDLWindow::GetRenderer(void);{
+    return this->renderer;
+}
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+{
+   printf("Erreur d'initialisation de la SDL : %s",SDL_GetError());//on affiche l'erreur
+   return EXIT_FAILURE;//on sort du programme pour éviter de plus gros problèmes
+}
+
+    MainSDLWindow* main_window = new MainSDLWindow;
+    main_window->Init("Snake", 600, 600);
+    SDL_Renderer* main_window_renderer = main_window->GetRenderer();
+
+    
+    SDL_RenderDrawRect;
 }
--- exercice1.hpp
+++ exercice1.hpp
@@ -1,18 +1,20 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 class MainSDLWindow
 {
 private:
-    
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
 public:
- MainSDLWindow();
-     MainSDLWindow();
+    MainSDLWindow();
+    
+    ~MainSDLWindow();
+
+    int Init(const char *windowName, int width, int height);
+
+    SDL_Renderer* GetRenderer(void);
 };
- MainSDLWindow: MainSDLWindow()
-{
-}
- MainSDLWindow:: MainSDLWindow()
-{
-}
