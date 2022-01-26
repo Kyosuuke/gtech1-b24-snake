26aeaa93839580fa96f4a9d47c131620fdf52ab2
Clement LI <cli@gaming.tech>
2022-01-26 14:43:28 +0100

    snakeclem

--- exercice1.cpp
+++ exercice1.cpp
@@ -1,27 +1,26 @@
-#include "snake.hpp"
+#include "exercice1.hpp"
 
-int MainSDLWindow::Init(const char *windowName, int width, int height){
+int MainSDLWindow::Init(const char* windowName, int width, int height)
+{
     this->window = SDL_CreateWindow(windowName,
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED, width , height , SDL_WINDOW_RESIZABLE);
     if(window == NULL) // Gestion des erreurs
- {
-    printf("Erreur lors de la creation d'une fenetre : %s",SDL_GetError());
-    return EXIT_FAILURE;
- }
+    {
+        printf("Erreur lors de la creation d'une fenetre : %s",SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer == NULL)//gestion des erreurs
+    {
+        printf("Erreur lors de la creation d'un renderer : %s",SDL_GetError());
+        return EXIT_FAILURE;
+    }
+  return 0; 
 }
-
 
 SDL_Renderer* MainSDLWindow::GetRenderer(void){
     return this->renderer;
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if(renderer == NULL)//gestion des erreurs
-{
-    printf("Erreur lors de la creation d'un renderer : %s",SDL_GetError());
-    return EXIT_FAILURE;
-}
-SDL_Delay(3000);//pause de 3 secondes
-
 }
 
 MainSDLWindow::MainSDLWindow(){
@@ -38,22 +37,21 @@ MainSDLWindow::~MainSDLWindow(){
     
 }
 
-int SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255){
-    return 0;
-}
-
 int main(void) {
     if(SDL_VideoInit(NULL) < 0) // Initialisation de la SDL
 {
     printf("Erreur d'initialisation de la SDL : %s",SDL_GetError());
     return EXIT_FAILURE;
 }
+    //Make sure the program waits for a quit
     MainSDLWindow* main_window = new MainSDLWindow;
     main_window->Init("Snake", 600, 600);
     SDL_Renderer* main_window_renderer = main_window->GetRenderer();
-    SDL_RenderDrawRect;
-    SDL_Delay(3000);//pause de 3 secondes
-    
-}
-
+    while (true){
+        SDL_SetRenderDrawColor(main_window_renderer, 0, 0, 0, 255);
+        SDL_RenderClear(main_window_renderer);
+        SDL_RenderDrawRect;
+        SDL_Quit();
+        return 0;
+    } 
 }
---
+++ snake.hpp
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+class MainSDLWindow
+{
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+public:
+    MainSDLWindow();
+    
+    ~MainSDLWindow();
+
+    int Init(const char *windowName, int width, int height);
+
+    SDL_Renderer* GetRenderer(void);
+};
