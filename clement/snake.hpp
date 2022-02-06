9a6e79c6677853bbae1abf33b3b457ce91ac6202
KeziahBartolini <92040185+KeziahBartolini@users.noreply.github.com>
2022-02-06 17:29:45 +0100

    ajouts pour fruit

--- clement/snake.hpp
+++ clement/snake.hpp
@@ -43,6 +43,10 @@ public :
     void keyboard();
     void move();
     void draw(SDL_Renderer* main_window_renderer);
+    
+    Segment* Snake::Gethead();
+    void Snake::Eat();
+    
 };
 
 #endif
